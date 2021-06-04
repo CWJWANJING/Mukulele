6fab3d9f4b01f5e206df3eab45ca992626009a87
Wanjing Chen <wanjingchencwj@gmail.com>
2021-06-04 16:33:29 +0100

    completed navigation bar and changed top tool bar

--- app/src/main/java/com/example/mukulele/MainActivity.java
+++ app/src/main/java/com/example/mukulele/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mukulele;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,8 +8,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -23,16 +27,33 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar = (Toolbar) findViewById(R.id.maintoolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-        Log.i("onCreate", "menu");
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main, menu);
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+////        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        Log.i("onCreate", "menu");
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//        String msg = "";
+//        // show people the score, extracts from Firebase
+//        switch (item.getItemId()){
+//            case R.id.ic_coins:
+//                msg = "number of coins";
+//                break;
+//            case R.id.ic_pet:
+//                msg = "cat's experience points";
+//                break;
+//        }
+//        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+//        return super.onOptionsItemSelected(item);
+//    }
 
     public void Logout(View view) {
         FirebaseAuth.getInstance().signOut();
