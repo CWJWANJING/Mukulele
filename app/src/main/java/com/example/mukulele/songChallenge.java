b97efb007bf4d99f0eab0284237e41e25552749b
Wanjing Chen <wanjingchencwj@gmail.com>
2021-06-16 16:01:09 +0100

    added basic function of pitch detection, but there are things need to adjust and take care of

--- app/src/main/java/com/example/mukulele/songChallenge.java
+++ app/src/main/java/com/example/mukulele/songChallenge.java
@@ -1,20 +1,40 @@
 package com.example.mukulele;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.q42.android.scrollingimageview.ScrollingImageView;
+
+import be.tarsos.dsp.AudioDispatcher;
+import be.tarsos.dsp.AudioEvent;
+import be.tarsos.dsp.AudioProcessor;
+import be.tarsos.dsp.io.android.AudioDispatcherFactory;
+import be.tarsos.dsp.pitch.PitchDetectionHandler;
+import be.tarsos.dsp.pitch.PitchDetectionResult;
+import be.tarsos.dsp.pitch.PitchProcessor;
 
 public class songChallenge extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
     ScrollingImageView scrollingBackground;
+    TextView pitchText;
+    TextView noteText;
+    AudioDispatcher dispatcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +68,130 @@ public class songChallenge extends AppCompatActivity {
 //        scrollingBackground.stop();
 //        scrollingBackground.start();
 
+        pitchText = findViewById(R.id.pitchText);
+        noteText = findViewById(R.id.noteText);
 
+        // Register the permissions callback, which handles the user's response to the
+// system permissions dialog. Save the return value, an instance of
+// ActivityResultLauncher, as an instance variable.
+        ActivityResultLauncher<String> requestPermissionLauncher =
+                registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                    if (isGranted) {
+
+                        // Permission is granted. Continue the action or workflow in your
+                        // app.
+                    } else {
+                        // Explain to the user that the feature is unavailable because the
+                        // features requires a permission that the user has denied. At the
+                        // same time, respect the user's decision. Don't link to system
+                        // settings in an effort to convince the user to change their
+                        // decision.
+                    }
+                });
+
+//        private void requestPermissionForMicrophone() {
+//            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+//                    Manifest.permission.RECORD_AUDIO)) {
+//                Toast.makeText(this,
+//                        R.string.permissions_needed,
+//                        Toast.LENGTH_LONG).show();
+//
+//            } else {
+//                requestPermissionLauncher.launch(
+//                        Manifest.permission.RECORD_AUDIO);
+//
+////                ActivityCompat.requestPermissions(
+////                        this,
+////                        new String[]{Manifest.permission.RECORD_AUDIO},
+////                        MIC_PERMISSION_REQUEST_CODE);
+//            }
+//        }
+
+
+        if (ContextCompat.checkSelfPermission(
+                this, Manifest.permission.RECORD_AUDIO) ==
+                PackageManager.PERMISSION_GRANTED) {
+            // You can use the API that requires the permission.
+            Log.i("i","permission granted");
+        } else if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECORD_AUDIO)) {
+            // In an educational UI, explain to the user why your app requires this
+            // permission for a specific feature to behave as expected. In this UI,
+            // include a "cancel" or "no thanks" button that allows the user to
+            // continue using your app without granting the permission.
+            Log.i("q","shouldShowRequestPermissionRationale");
+        } else {
+            // You can directly ask for the permission.
+            // The registered ActivityResultCallback gets the result of this request.
+            requestPermissionLauncher.launch(
+                    Manifest.permission.RECORD_AUDIO);
+        }
+
+        // The following code is from
+        // https://medium.com/@juniorbump/pitch-detection-in-android-using-tarsosdsp-a2dd4a3f04e9
+        // TarsosDSP comes equipped with its microphone detection
+        dispatcher =
+                AudioDispatcherFactory.fromDefaultMicrophone(22050,1024,0);
+
+        //TarsosDSP has a simple PitchDetection class
+        PitchDetectionHandler pdh = new PitchDetectionHandler() {
+            @Override
+            public void handlePitch(PitchDetectionResult res, AudioEvent e){
+                final float pitchInHz = res.getPitch();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.i("w","Inside run");
+                        processPitch(pitchInHz);
+                    }
+                });
+            }
+        };
+        AudioProcessor pitchProcessor = new PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.FFT_YIN, 22050, 1024, pdh);
+        dispatcher.addAudioProcessor(pitchProcessor);
+
+        Thread audioThread = new Thread(dispatcher, "Audio Thread");
+        audioThread.start();
+
+    }
+
+
+
+    // This function is also from
+    // https://medium.com/@juniorbump/pitch-detection-in-android-using-tarsosdsp-a2dd4a3f04e9
+    // This will display the pitch and convert to notes
+    public void processPitch(float pitchInHz) {
+
+        pitchText.setText("" + pitchInHz);
+        Log.i("p", String.valueOf(pitchInHz));
+
+        if(pitchInHz >= 110 && pitchInHz < 123.47) {
+            //A
+            noteText.setText("A");
+        }
+        else if(pitchInHz >= 123.47 && pitchInHz < 130.81) {
+            //B
+            noteText.setText("B");
+        }
+        else if(pitchInHz >= 130.81 && pitchInHz < 146.83) {
+            //C
+            noteText.setText("C");
+        }
+        else if(pitchInHz >= 146.83 && pitchInHz < 164.81) {
+            //D
+            noteText.setText("D");
+        }
+        else if(pitchInHz >= 164.81 && pitchInHz <= 174.61) {
+            //E
+            noteText.setText("E");
+        }
+        else if(pitchInHz >= 174.61 && pitchInHz < 185) {
+            //F
+            noteText.setText("F");
+        }
+        else if(pitchInHz >= 185 && pitchInHz < 196) {
+            //G
+            noteText.setText("G");
+        }
     }
 
 }
